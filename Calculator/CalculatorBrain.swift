b1c80107363f6816b9b75b1340a12d3f89db4e9e
appcamp <appcamp@systemss-MacBook-Pro.local>
2015-07-19 14:00:13 +0200

    no message

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+class CalculatorBrain {
+    var operation: String = "="
+    var operand1: Double = 0
+    var operand2: Double = 0
+    
+    func calculate() -> Double {
+        switch operation {
+            case "+": return operand2 + operand1
+            case "-": return operand1 - operand2
+            case "*": return operand2 * operand1
+            case "/":
+                if operand1.isZero==false {
+                    return operand1 / operand2
+                }
+                else {
+                    return 0
+                }
+            default: return 0
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    var isUserInTheMiddleOfTypingNumber: Bool = false
+    var displayValue: Double {
+        get {
+            return (display.text! as NSString).doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    let brain: CalculatorBrain = CalculatorBrain()
+    
+    @IBAction func pushDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if isUserInTheMiddleOfTypingNumber {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit
+            isUserInTheMiddleOfTypingNumber = true
+        }
     }
-
-
+    
+    
+    @IBAction func pushOperation(sender: UIButton) {
+        isUserInTheMiddleOfTypingNumber = false
+        brain.operation = sender.currentTitle!
+        brain.operand1 = displayValue
+    }
+    
+    @IBAction func pushEquals() {
+        brain.operand2 = displayValue
+        displayValue = brain.calculate()
+        isUserInTheMiddleOfTypingNumber = false
+    
+    }
+    
+    @IBAction func clearDisplay(sender: UIButton) {
+        display.text = "0"
+        displayValue = 0
+        brain.operand1 = 0
+    }
+    
+    @IBAction func pushDot(sender: UIButton) {
+        if isUserInTheMiddleOfTypingNumber {
+            if display.text!.rangeOfString(".") == nil {
+                display.text = display.text! + "."
+            }
+        }
+    }
 }
 
